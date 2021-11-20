e3a2c78613cde9c3a98f7c1eaafa67cd29297fac
VIDEN22014 <dxbogdan4@gmail.com>
2021-11-20 17:34:15 +0200

    added 3 empty levels

--- proj.win32/Level1Scene.cpp
+++ proj.win32/Level1Scene.cpp
@@ -1,4 +1,6 @@
+#include "ui/CocosGUI.h"
 #include <proj.win32/Level1Scene.h>
+#include <proj.win32/Game.h>
 
 USING_NS_CC;
 
@@ -11,17 +13,40 @@ Scene* Level1Scene::createScene()
 
 bool Level1Scene::init()
 {
-
 	if (!Scene::init())
 	{
 		return false;
 	}
-
-	auto sprite = Sprite::create("backmenu.png");
+	//Background Sprite
+	auto spriteBackground = Sprite::create("Assets/Backgrounds/BG_4_960.png");
 	Size size = Director::getInstance()->getWinSize();
-	sprite->setAnchorPoint(Vec2(0, 0));
-	sprite->setPosition(Vec2(0, 0));
-	this->addChild(sprite);
+	spriteBackground->setAnchorPoint(Vec2(0, 0));
+	spriteBackground->setPosition(Vec2(0, 0));
+	this->addChild(spriteBackground);
+
+
+
+
+	//Return Button
+	auto returnButton = ui::Button::create("Assets/UI/StoneButtonsLight/tile005.png", "Assets/UI/StoneButtonsLightPressed/tile005.png");
+	returnButton->setScale(5);
+	returnButton->setAnchorPoint(Vec2(0, 0));
+	returnButton->setPosition(Vec2(8 * returnButton->getScale(), 8 * returnButton->getScale()));
+	returnButton->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+		switch (type)
+		{
+		case ui::Widget::TouchEventType::BEGAN:
+			break;
+		case ui::Widget::TouchEventType::ENDED:
+			Game::GoToLevelSelect();
+			break;
+		default:
+			break;
+		}
+		});
+	this->addChild(returnButton);
+
+
 
 	return true;
 }
--- proj.win32/Level3Scene.cpp
+++ proj.win32/Level3Scene.cpp
@@ -1,4 +1,6 @@
+#include "ui/CocosGUI.h"
 #include <proj.win32/Level3Scene.h>
+#include <proj.win32/Game.h>
 
 USING_NS_CC;
 
@@ -16,12 +18,36 @@ bool Level3Scene::init()
 	{
 		return false;
 	}
-
-	auto sprite = Sprite::create("backmenu.png");
+	//Background Sprite
+	auto spriteBackground = Sprite::create("Assets/Backgrounds/BG_4_960.png");
 	Size size = Director::getInstance()->getWinSize();
-	sprite->setAnchorPoint(Vec2(0, 0));
-	sprite->setPosition(Vec2(0, 0));
-	this->addChild(sprite);
+	spriteBackground->setAnchorPoint(Vec2(0, 0));
+	spriteBackground->setPosition(Vec2(0, 0));
+	this->addChild(spriteBackground);
+
+
+
+
+	//Return Button
+	auto returnButton = ui::Button::create("Assets/UI/StoneButtonsLight/tile005.png", "Assets/UI/StoneButtonsLightPressed/tile005.png");
+	returnButton->setScale(5);
+	returnButton->setAnchorPoint(Vec2(0, 0));
+	returnButton->setPosition(Vec2(8 * returnButton->getScale(), 8 * returnButton->getScale()));
+	returnButton->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+		switch (type)
+		{
+		case ui::Widget::TouchEventType::BEGAN:
+			break;
+		case ui::Widget::TouchEventType::ENDED:
+			Game::GoToLevelSelect();
+			break;
+		default:
+			break;
+		}
+		});
+	this->addChild(returnButton);
+
+
 
 	return true;
 }
