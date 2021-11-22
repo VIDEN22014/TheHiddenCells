318efe626a3c389fb852125bc42be5c719a0f06e
VIDEN22014 <dxbogdan4@gmail.com>
2021-11-22 18:47:55 +0200

    123

--- proj.win32/Game.h
+++ proj.win32/Game.h
@@ -12,6 +12,20 @@ namespace gameData {
 	extern int priceAmmo;
 }
 
+struct position
+{
+	//позиції як в двоимірному масиві
+	int x;
+	int y;
+
+	position() {}
+	position(int X, int Y) {
+		x = X;
+		y = Y;
+	}
+};
+
+
 class Game
 {
 public:
