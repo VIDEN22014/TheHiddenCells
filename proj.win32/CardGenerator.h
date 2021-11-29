fe4c2acf77fb9ec5511f75ce896a651002e32f36
max <maksymchukmaxym@gmail.com>
2021-11-29 20:39:36 +0200

    class CardGenerator

---
+++ proj.win32/CardGenerator.h
@@ -0,0 +1,14 @@
+#include "cocos2d.h"
+#include <proj.win32/Game.h>
+#include <proj.win32/Cards.h>
+
+class CardGenerator {
+public:
+	CardGenerator(int lvl) {};
+	AbstractFactory*  Frame;
+
+	Card* GenerateRandomCard(position pos) {};
+	Card* GenerateGoodCard(position pos) {};
+	Card* GenerateBadCard(position pos) {};
+
+};
